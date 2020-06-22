866293ef77097208c0c440d62870344d0c2e8e04
NataliaRydza <natalie.rydza@gmail.com>
2020-06-22 20:26:02 +0200

    Itinerary - styling changes

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,24 +1,18 @@
 <template>
-  <div>
-    <GmapMap
-      :center="{ lat: 49.9, lng: 15.25 }"
-      :zoom="10"
-      map-type-id="terrain"
-    >
-      <GmapMarker
-        :key="index"
-        v-for="(m, index) in places"
-        :position="{ lat: m.latitude, lng: m.longitude }"
-        :clickable="true"
-        :animation="google && google.maps.Animation.DROP"
-      />
-      <gmap-polyline
-        v-if="path.length > 0"
-        :path="path"
-        ref="polyline"
-      ></gmap-polyline>
-    </GmapMap>
-  </div>
+  <GmapMap :center="{ lat: 49.9, lng: 15.25 }" :zoom="10" map-type-id="terrain">
+    <GmapMarker
+      :key="index"
+      v-for="(m, index) in places"
+      :position="{ lat: m.latitude, lng: m.longitude }"
+      :clickable="true"
+      :animation="google && google.maps.Animation.DROP"
+    />
+    <gmap-polyline
+      v-if="path.length > 0"
+      :path="path"
+      ref="polyline"
+    ></gmap-polyline>
+  </GmapMap>
 </template>
 <script>
 import { gmapApi } from "gmap-vue";
@@ -39,13 +33,7 @@ export default {
 </script>
 <style scoped>
 .vue-map-container {
-  width: 100vw;
-  height: 300px;
-}
-@media screen and (min-width: 1310px) {
-  .vue-map-container {
-    width: 450px;
-    height: calc(100vh - 116px);
-  }
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="search">
-    <div class="pagePosition">
+    <div class="container pagePosition">
       <div class="checkboxSection" id="checkboxes">
-        <b-form-select class="margin" v-model="selectedCity" required>
+        <b-form-select v-model="selectedCity" required>
           <b-form-select-option hidden disabled value
             >Prosím vyberte</b-form-select-option
           >
@@ -14,7 +14,7 @@
           >
         </b-form-select>
 
-        <b-dropdown class="mx-1" right text="Historické památky">
+        <b-dropdown right text="Historie">
           <b-dropdown-text>
             <label for="church">
               <input
@@ -39,7 +39,7 @@
           </b-dropdown-text>
         </b-dropdown>
 
-        <b-dropdown class="mx-1" right text="Relax">
+        <b-dropdown right text="Relax">
           <b-dropdown-text>
             <label for="cinema">
               <input
@@ -70,7 +70,7 @@
           </b-dropdown-text>
         </b-dropdown>
 
-        <b-dropdown class="mx-1" right text="Rodinné aktivity">
+        <b-dropdown right text="Rodinné aktivity">
           <b-dropdown-text>
             <label for="kids">
               <input type="checkbox" id="kids" value="kids" v-model="filters" />
@@ -91,7 +91,7 @@
           </b-dropdown-text>
         </b-dropdown>
 
-        <b-dropdown class="mx-1" right text="Sportovní aktivity">
+        <b-dropdown right text="Sport">
           <b-dropdown-text>
             <label for="swim">
               <input type="checkbox" id="swim" value="swim" v-model="filters" />
@@ -107,59 +107,73 @@
           </b-dropdown-text>
         </b-dropdown>
       </div>
-      <div id="checkboxesOutput">
-        <div v-for="place in places" :key="place.name" class="outputContainer">
-          <div class="outputCheckbox">
-            <input
-              type="checkbox"
-              :id="place.name"
-              :value="place.name"
-              v-model="selection"
-            />
-          </div>
-          <div class="wholeOutput">
-            <label :for="place.name">
-              <h2>{{ place.name }}</h2>
-              <p>{{ place.outputAddress }}</p>
-              <p>{{ place.outputDescription }}</p>
-              <hr />
-            </label>
-          </div>
-        </div>
-        <div class="links">
-          <div class="linksContainer" v-if="mapPlaces.length >= 2">
-            <a
-              :href="generateGoogleMapLink('walking')"
-              target="_blank"
-              class="outputButton"
-              >Idem pešo!</a
-            >
-            <a
-              :href="generateGoogleMapLink('transit')"
-              target="_blank"
-              class="outputButton"
-              >Idem MHD!</a
-            >
-            <a
-              :href="generateGoogleMapLink('driving')"
-              target="_blank"
-              class="outputButton"
-              >Idem autom!</a
-            >
-          </div>
-        </div>
-      </div>
 
-      <div class="mapDisplay">
-        <Map :places="mapPlaces" />
+      <div class="row searchResults">
+        <div class="col-md" id="checkboxesOutput">
+          <div v-if="places.length === 0">
+            Prosim vyberte nekterou z kategorii.
+          </div>
+          <div
+            v-for="place in places"
+            :key="place.name"
+            class="outputContainer"
+          >
+            <div class="outputCheckbox">
+              <input
+                type="checkbox"
+                :id="place.name"
+                :value="place.name"
+                v-model="selection"
+              />
+            </div>
+            <div class="wholeOutput">
+              <label :for="place.name">
+                <h2>{{ place.name }}</h2>
+                <p>{{ place.outputAddress }}</p>
+                <p>{{ place.outputDescription }}</p>
+                <hr />
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md mapDisplay">
+          <div class="links">
+            <div class="row" v-if="mapPlaces.length >= 2">
+              <div class="col-sm">
+                <a
+                  :href="generateGoogleMapLink('walking')"
+                  target="_blank"
+                  class="outputButton"
+                  >Idem pešo!</a
+                >
+              </div>
+              <div class="col-sm">
+                <a
+                  :href="generateGoogleMapLink('transit')"
+                  target="_blank"
+                  class="outputButton"
+                  >Idem MHD!</a
+                >
+              </div>
+              <div class="col-sm">
+                <a
+                  :href="generateGoogleMapLink('driving')"
+                  target="_blank"
+                  class="outputButton"
+                  >Idem autom!</a
+                >
+              </div>
+            </div>
+          </div>
+          <Map :places="mapPlaces" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
 import { db } from "../db";
-// import { cities } from "../placesData.js";
-// import { places } from "../placesItinerary.js";
 import Map from "./Map.vue";
 export default {
   name: "Search",
@@ -228,35 +242,45 @@ export default {
   components: { Map },
 };
 </script>
+<style>
+.search .dropdown .btn {
+  background-color: #ec7568;
+  border: 1px #ec7568 solid;
+}
+.search .show > .btn.dropdown-toggle {
+  background-color: #ec7568;
+  border: 1px #ec7568 solid;
+}
+</style>
 <style scoped>
+.searchResults {
+  margin-top: 30px;
+}
+.custom-select {
+  width: inherit;
+}
+.search .dropdown {
+  margin: 3px 0;
+}
 .outputContainer {
   display: flex;
 }
 .wholeOutput {
   padding: 0 0 10px 10px;
 }
-.mapDisplay {
-  display: inline-block;
-  margin-top: 15px;
-}
+
 .outputCheckbox {
   margin: 9px;
 }
 .checkboxSection {
-  padding-bottom: 80px;
-  font-family: sans-serif;
   background-color: rgb(254, 250, 242);
 }
 .mx-1 {
-  margin: 1px;
+  margin: 3px 0;
 }
-.margin {
-  margin-left: 15px;
-  max-width: 160px;
-}
-#checkboxes,
-#checkboxesOutput {
-  width: 100vw;
+
+#checkboxes {
+  text-align: center;
 }
 #checkboxesOutput {
   padding: 20px;
@@ -270,23 +294,24 @@ export default {
   cursor: pointer;
   padding: 0.25rem 1.5rem;
   display: block;
+  font-weight: 400;
 }
 
 .outputButton {
   text-decoration: none;
-  color: #6c757d;
-  padding: 20px;
+  background-color: #ec7568;
+  color: white;
+  padding: 5px;
   cursor: pointer;
-  border: 1px #6c757d solid;
-  border-radius: 20px;
+  display: block;
+  text-align: center;
+  border: 1px #ec7568 solid;
+  border-radius: 5px;
+  margin-bottom: 10px;
 }
 .outputButton:hover {
   background-color: #6c757d;
   color: white;
-}
-.linksContainer {
-  display: flex;
-  justify-content: space-around;
 }
 
 hr {
@@ -295,25 +320,26 @@ hr {
 .wholeOutput p {
   padding-right: 15px;
 }
+.mapDisplay {
+  min-height: 300px;
+}
+
 @media screen and (max-width: 810px) {
   #checkboxes {
     display: flex;
     flex-direction: column;
   }
 }
-@media screen and (min-width: 811px) and (max-width: 1310px) {
-  .checkboxSection {
-    padding-bottom: 100px;
-  }
-}
-
-@media screen and (min-width: 1310px) {
+@media screen and (min-width: 811px) {
   .mapDisplay {
-    position: fixed;
-    right: 0;
-    bottom: 0;
-    margin: 0;
+    background-color: rgb(254, 250, 242);
+    /* overlay the left column */
+    margin-top: 0px;
+    margin-left: -30px;
+
+    min-height: calc(100vh - 116px - 94px - 30px);
   }
+
   select:invalid {
     color: grey;
   }
@@ -323,30 +349,16 @@ hr {
 
   #checkboxesOutput {
     width: calc(100vw - 400px);
-    margin-top: -100px;
-    max-height: calc(100vh - 116px - 120px);
+
+    max-height: calc(100vh - 116px - 94px - 30px);
     overflow-y: scroll;
     overflow-x: hidden;
   }
-  .checkboxSection {
-    width: calc(100vw - 400px);
-    overflow-y: scroll;
-    height: 220px;
-    padding: 25px;
-    display: flex;
-    align-items: flex-start;
-    padding-right: 460px;
-  }
+
   select {
-    font-size: 20px;
+    font-size: 16px;
   }
-  #checkboxes {
-    display: flex;
-    flex-direction: row;
-  }
-  .linksContainer {
-    width: calc(100vw - 470px);
-  }
+
   .wholeOutput p,
   .wholeOutput h2 {
     padding-right: 45px;
